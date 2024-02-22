b59ef593f2a0ef9fc24f069f736fe60d4ad8e309
Hatem Yakoubi <hatemyakoubi15@gmail.com>
2024-02-22 22:37:32 +0100

    integrate template and create db

---
+++ Models/Gouvernorat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project.NET.Models;
+
+public partial class Gouvernorat
+{
+    public int Id { get; set; }
+
+    public string Identifiant { get; set; } = null!;
+
+    public string Libelle { get; set; } = null!;
+}
---
+++ Models/Recette.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project.NET.Models;
+
+public partial class Recette
+{
+    public int Id { get; set; }
+
+    public int? CodeCentreId { get; set; }
+
+    public string Code { get; set; } = null!;
+
+    public string Libelle { get; set; } = null!;
+
+    public virtual Structure? CodeCentre { get; set; }
+}
